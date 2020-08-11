47584ddb302d4f99eb0d5c93cf668e0808b28d40
Lev Karpov <nt66@bk.ru>
2020-08-11 17:28:46 +0300

    оптимизация модалок

--- src/js/open-windows.js
+++ src/js/open-windows.js
@@ -15,19 +15,27 @@ const callbackBtn = document.querySelectorAll('.callback');
 const modalCallback = document.querySelector('.modal__callback');
 const modalCloseCallback = document.querySelector('.modal__close-callback');
 
+//закрывает модалки
+
+function closeModal() {
+  sideMenu.classList.remove('side__menu--active');
+  wrapperOpenMenu.classList.remove('wrapper__open-menu-bg');
+  navigationOpenBtn.classList.remove('header__navigation-toggle-btn-close');
+  searchList.classList.remove('header__search-list-open');
+  informationsList.classList.remove('information__list-open-menu');
+  contactsList.classList.remove('contacts__list-open-menu');
+  modalFeedback.classList.remove('modal__feedback-open');
+  modalCallback.classList.remove('modal__callback-open');
+  wrapperOpenMenu.style.zIndex = '9';
+  document.removeEventListener('keydown', modalEscPressHandler);
+}
+
 //закрытие на ESC
 
 function modalEscPressHandler(evt) {
   if (evt.keyCode === 27) {
-    sideMenu.classList.remove('side__menu--active');
-    wrapperOpenMenu.classList.remove('wrapper__open-menu-bg');
-    navigationOpenBtn.classList.remove('header__navigation-toggle-btn-close');
-    searchList.classList.remove('header__search-list-open');
-    informationsList.classList.remove('information__list-open-menu');
-    contactsList.classList.remove('contacts__list-open-menu');
-    modalFeedback.classList.remove('modal__feedback-open');
-    modalCallback.classList.remove('modal__callback-open');
-    wrapperOpenMenu.style.zIndex = '9';
+    console.log('1');
+    closeModal()
   }
 };
 
@@ -40,90 +48,31 @@ function navigationOpenBtnClickHandler() {
   searchList.classList.toggle('header__search-list-open');
   contactsList.classList.toggle('contacts__list-open-menu');
   informationsList.classList.toggle('information__list-open-menu');
-}
-
-//работает с обработкой событий при клике на фон
-
-function wrapperOpenMenuHandler() {
-  sideMenu.classList.remove('side__menu--active');
-  wrapperOpenMenu.classList.remove('wrapper__open-menu-bg');
-  navigationOpenBtn.classList.remove('header__navigation-toggle-btn-close');
-  searchList.classList.remove('header__search-list-open');
-  informationsList.classList.remove('information__list-open-menu');
-  contactsList.classList.remove('contacts__list-open-menu');
-  modalFeedback.classList.remove('modal__feedback-open');
-  modalCallback.classList.remove('modal__callback-open');
-  wrapperOpenMenu.style.zIndex = '9';
+  document.addEventListener('keydown', modalEscPressHandler);
 }
 
 navigationOpenBtn.addEventListener('click', navigationOpenBtnClickHandler); 
 
-wrapperOpenMenu.addEventListener('click', wrapperOpenMenuHandler);
-
-document.addEventListener('keydown', modalEscPressHandler);
+wrapperOpenMenu.addEventListener('click', closeModal);
  
-/*if (!navigationOpenBtn.classList.contains('header__navigation-toggle-btn-close')) {
-    sideMenu.classList.add('side__menu-active');
-    wrapperOpenMenu.classList.add('wrapper__open-menu-bg');
-    navigationOpenBtn.classList.add('header__navigation-toggle-btn-close');
-    searchList.classList.add('header__search-list-open');
-    contactsList.classList.add('contacts__list-open-menu');
-    informationsList.classList.add('information__list-open-menu');
+//функции для модалок
+
+function wrapperActionHandler() {
+  wrapperOpenMenu.classList.remove('wrapper__open-menu-bg');
+  if (!navigationOpenBtn.classList.contains('header__navigation-toggle-btn-close')) {
+    wrapperOpenMenu.style.zIndex = '0';
   } else {
-    navigationOpenBtn.classList.remove('header__navigation-toggle-btn-close');
-    sideMenu.classList.remove('side__menu-active');
-    wrapperOpenMenu.classList.remove('wrapper__open-menu-bg');
-    searchList.classList.remove('header__search-list-open');
-    contactsList.classList.remove('contacts__list-open-menu');
-    informationsList.classList.remove('information__list-open-menu');
-  }*/
-
-// }); 
-
-
-
-/*function () {
-    sideMenu.classList.remove('side__menu-active');
-    wrapperOpenMenu.classList.remove('wrapper__open-menu-bg');
-    navigationOpenBtn.classList.remove('header__navigation-toggle-btn-close');
-    searchList.classList.remove('header__search-list-open');
-    informationsList.classList.remove('information__list-open-menu');
-    contactsList.classList.remove('contacts__list-open-menu');
-    modalFeedback.style.display = 'none';
-    modalCallback.style.display = 'none';
-    wrapperOpenMenu.style.zIndex = '9';
-    modalFeedback.style.display = 'none';
-    modalCallback.style.display = 'none';
-});*/
-
-
-// функция появления/исчезания хедера при ресайзе экрана
-
-/*function onOpen() {
-  if (window.innerWidth >= '1440') {
-    sideMenu.classList.remove('side__menu-active');
-    navigationOpenBtn.classList.remove('header__navigation-toggle-btn-close');
-    wrapperOpenMenu.classList.remove('wrapper__open-menu-bg');
-    searchList.classList.remove('header__search-list-open');
-    contactsList.classList.remove('contacts__list-open-menu');
-    informationsList.classList.remove('information__list-open-menu');
+      wrapperOpenMenu.style.zIndex = '9';
+      wrapperOpenMenu.classList.add('wrapper__open-menu-bg');
   }
 }
-
-onOpen();
-
-window.addEventListener('resize', function () {
-  onOpen();
-});*/
-
-
-//функции для модалок
 
 function feedbackBtnOpenHandler() {
   modalFeedback.classList.add('modal__feedback-open');
   wrapperOpenMenu.classList.add('wrapper__open-menu-bg');
   wrapperOpenMenu.style.zIndex = '999';
   modalCloseFeedback.focus();
+  document.addEventListener('keydown', modalEscPressHandler);
 }
 
 function callbackBtnOpenHandler() {
@@ -131,30 +80,18 @@ function callbackBtnOpenHandler() {
   wrapperOpenMenu.classList.add('wrapper__open-menu-bg');
   wrapperOpenMenu.style.zIndex = '999';
   modalCloseCallback.focus();
+  document.addEventListener('keydown', modalEscPressHandler);
 }
 
 function modalCloseFeedbackHandler() {
   modalFeedback.classList.remove('modal__feedback-open');
-  wrapperOpenMenu.classList.remove('wrapper__open-menu-bg');
-  if (!navigationOpenBtn.classList.contains('header__navigation-toggle-btn-close')) {
-    wrapperOpenMenu.style.zIndex = '0';
-  } else {
-      wrapperOpenMenu.style.zIndex = '9';
-      wrapperOpenMenu.classList.add('wrapper__open-menu-bg');
-  }
+  wrapperActionHandler();
 }
 
 function modalCloseCallbackHandler() {
   modalCallback.classList.remove('modal__callback-open');
-  wrapperOpenMenu.classList.remove('wrapper__open-menu-bg');
-  if (!navigationOpenBtn.classList.contains('header__navigation-toggle-btn-close')) {
-    wrapperOpenMenu.style.zIndex = '0';
-  } else {
-      wrapperOpenMenu.style.zIndex = '9';
-      wrapperOpenMenu.classList.add('wrapper__open-menu-bg');
-  }
+  wrapperActionHandler();
 }
-
 
 for (let j = 0; j < feedbackBtn.length; j++) {
   feedbackBtn[j].addEventListener('click',feedbackBtnOpenHandler);
@@ -167,43 +104,3 @@ for (let i = 0; i < callbackBtn.length; i++) {
 modalCloseCallback.addEventListener('click', modalCloseCallbackHandler);
 
 modalCloseFeedback.addEventListener('click', modalCloseFeedbackHandler);
-  
-  /*function () {
-    modalFeedback.classList.add('modal__feedback-open');
-    wrapperOpenMenu.classList.add('wrapper__open-menu-bg');
-    wrapperOpenMenu.style.zIndex = '999';
-  });
-}*/
-
-
-
-/*function () {
-  modalFeedback.classList.remove('modal__feedback-open');
-  wrapperOpenMenu.classList.remove('wrapper__open-menu-bg');
-  if (!navigationOpenBtn.classList.contains('header__navigation-toggle-btn-close')) {
-    wrapperOpenMenu.style.zIndex = '0';
-  } else {
-      wrapperOpenMenu.style.zIndex = '9';
-      wrapperOpenMenu.classList.add('wrapper__open-menu-bg');
-  }
-});*/
-
-  
-  /*function () {
-    modalCallback.classList.add('modal__callback-open');
-    wrapperOpenMenu.classList.add('wrapper__open-menu-bg');
-    wrapperOpenMenu.style.zIndex = '999';
-  });
-}*/
-
-
-/*function () {
-  modalCallback.classList.remove('modal__callback-open');
-  wrapperOpenMenu.classList.remove('wrapper__open-menu-bg');
-  if (!navigationOpenBtn.classList.contains('header__navigation-toggle-btn-close')) {
-    wrapperOpenMenu.style.zIndex = '0';
-  } else {
-      wrapperOpenMenu.style.zIndex = '9';
-      wrapperOpenMenu.classList.add('wrapper__open-menu-bg');
-  }
-});*/
