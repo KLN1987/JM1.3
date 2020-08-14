8c8a4e5acb1cb9178ecb80ca26f0c35e49eeb0d6
Lev Karpov <nt66@bk.ru>
2020-08-14 17:19:56 +0300

    оптимизация модалок

--- src/js/open-windows.js
+++ src/js/open-windows.js
@@ -1,106 +1,98 @@
 const navigationOpenBtn = document.querySelector('.header__navigation-toggle-btn');
-const navigationCloseBtn = document.querySelector('.header__navigation-toggle-btn-close');
-const sideMenu = document.querySelector('.side__menu');
-const wrapperOpenMenu = document.querySelector('.wrapper__open-menu');
-const wrapperOpenMenuBg = document.querySelector('.wrapper__open-menu-bg');
-const searchList = document.querySelector('.header__search-list');
-const informationsList = document.querySelector('.informations__list');
-const contactsList = document.querySelector('.contacts__list');
+const navigationCloseBtn = document.querySelector('.side-menu__toggle-btn');
+const sideMenu = document.querySelector('.side-menu');
+const wrapperOpenMenu = document.querySelector('.main__wrapper-open-menu');
 
-const feedbackBtn = document.querySelectorAll('.feedback');
-const modalFeedback = document.querySelector('.modal__feedback');
-const modalCloseFeedback = document.querySelector('.modal__close-feedback');
+const modalFeedbackOpenBtn = document.querySelectorAll('.contacts__link--feedback');
+const modalFeedback = document.querySelector('.modal_feedback');
+const modalFeedbackCloseBtn = document.querySelector('.modal__close-feedback');
 
-const callbackBtn = document.querySelectorAll('.callback');
-const modalCallback = document.querySelector('.modal__callback');
-const modalCloseCallback = document.querySelector('.modal__close-callback');
+const modalCallbackOpenBtn = document.querySelectorAll('.contacts__link--callback');
+const modalCallback = document.querySelector('.modal_callback');
+const modalCallbackCloseBtn = document.querySelector('.modal__close-callback');
 
-//закрывает модалки
-
-function closeModal() {
-  sideMenu.classList.remove('side__menu--active');
-  wrapperOpenMenu.classList.remove('wrapper__open-menu-bg');
-  navigationOpenBtn.classList.remove('header__navigation-toggle-btn-close');
-  searchList.classList.remove('header__search-list-open');
-  informationsList.classList.remove('information__list-open-menu');
-  contactsList.classList.remove('contacts__list-open-menu');
-  modalFeedback.classList.remove('modal__feedback-open');
-  modalCallback.classList.remove('modal__callback-open');
-  wrapperOpenMenu.style.zIndex = '9';
-  document.removeEventListener('keydown', modalEscPressHandler);
-}
-
-//закрытие на ESC
-
-function modalEscPressHandler(evt) {
-  if (evt.keyCode === 27) {
-    console.log('1');
-    closeModal()
-  }
-};
+const feedbackInputFocus = modalFeedback.querySelector('.form__input');
+const callbackInputFocus = modalCallback.querySelector('.form__input');
 
 // открывает боковое меню
-
-function navigationOpenBtnClickHandler() { 
-  sideMenu.classList.toggle('side__menu--active');
-  wrapperOpenMenu.classList.toggle('wrapper__open-menu-bg');
-  navigationOpenBtn.classList.toggle('header__navigation-toggle-btn-close');
-  searchList.classList.toggle('header__search-list-open');
-  contactsList.classList.toggle('contacts__list-open-menu');
-  informationsList.classList.toggle('information__list-open-menu');
-  document.addEventListener('keydown', modalEscPressHandler);
+function openedSideMenu() {
+  sideMenu.classList.add('side-menu__active');
+  wrapperOpenMenu.classList.add('main__wrapper-open-menu--active');
+  document.addEventListener('keydown', escapeKeydownHandler);
 }
 
-navigationOpenBtn.addEventListener('click', navigationOpenBtnClickHandler); 
-
-wrapperOpenMenu.addEventListener('click', closeModal);
+// закрывает боковое меню
+function closedSideMenu() {
+  sideMenu.classList.remove('side-menu__active');
+  wrapperOpenMenu.classList.remove('main__wrapper-open-menu--active');
+}
  
 //функции для модалок
-
-function wrapperActionHandler() {
-  wrapperOpenMenu.classList.remove('wrapper__open-menu-bg');
-  if (!navigationOpenBtn.classList.contains('header__navigation-toggle-btn-close')) {
-    wrapperOpenMenu.style.zIndex = '0';
-  } else {
-      wrapperOpenMenu.style.zIndex = '9';
-      wrapperOpenMenu.classList.add('wrapper__open-menu-bg');
+function closeModalBg() {
+  if (sideMenu.classList.contains('side-menu__active')) {
+    wrapperOpenMenu.style.zIndex = '9';
+ } else {
+  wrapperOpenMenu.classList.remove('main__wrapper-open-menu--active');
+  wrapperOpenMenu.removeAttribute('style');
   }
 }
 
 function feedbackBtnOpenHandler() {
-  modalFeedback.classList.add('modal__feedback-open');
-  wrapperOpenMenu.classList.add('wrapper__open-menu-bg');
-  wrapperOpenMenu.style.zIndex = '999';
-  modalCloseFeedback.focus();
-  document.addEventListener('keydown', modalEscPressHandler);
+  modalFeedback.classList.add('modal_feedback--active');
+  wrapperOpenMenu.classList.add('main__wrapper-open-menu--active');
+  wrapperOpenMenu.style.zIndex = '99';
+  feedbackInputFocus.focus();
+  document.addEventListener('keydown', escapeKeydownHandler);
 }
 
 function callbackBtnOpenHandler() {
-  modalCallback.classList.add('modal__callback-open');
-  wrapperOpenMenu.classList.add('wrapper__open-menu-bg');
-  wrapperOpenMenu.style.zIndex = '999';
-  modalCloseCallback.focus();
-  document.addEventListener('keydown', modalEscPressHandler);
+  modalCallback.classList.add('modal_callback--active');
+  wrapperOpenMenu.classList.add('main__wrapper-open-menu--active');
+  wrapperOpenMenu.style.zIndex = '99';
+  callbackInputFocus.focus();
+  document.addEventListener('keydown', escapeKeydownHandler);
 }
 
-function modalCloseFeedbackHandler() {
-  modalFeedback.classList.remove('modal__feedback-open');
-  wrapperActionHandler();
+function modalFeedbackCloseBtnHandler() {
+  modalFeedback.classList.remove('modal_feedback--active');
+  closeModalBg();
 }
 
-function modalCloseCallbackHandler() {
-  modalCallback.classList.remove('modal__callback-open');
-  wrapperActionHandler();
+function modalCallbackCloseBtnHandler() {
+  modalCallback.classList.remove('modal_callback--active');
+  closeModalBg();
 }
 
-for (let j = 0; j < feedbackBtn.length; j++) {
-  feedbackBtn[j].addEventListener('click',feedbackBtnOpenHandler);
+for (let j = 0; j < modalFeedbackOpenBtn.length; j++) {
+  modalFeedbackOpenBtn[j].addEventListener('click',feedbackBtnOpenHandler);
 } 
 
-for (let i = 0; i < callbackBtn.length; i++) {
-  callbackBtn[i].addEventListener('click', callbackBtnOpenHandler)
+for (let i = 0; i < modalCallbackOpenBtn.length; i++) {
+  modalCallbackOpenBtn[i].addEventListener('click', callbackBtnOpenHandler)
 } 
 
-modalCloseCallback.addEventListener('click', modalCloseCallbackHandler);
+//фнкция закрытия при esc и клике по фону
+function closeModal() {
+  if (modalFeedback.classList.contains('modal_feedback--active') && wrapperOpenMenu.style.zIndex === '99') {
+    return modalFeedbackCloseBtnHandler();
+  } if (modalCallback.classList.contains('modal_callback--active') && wrapperOpenMenu.style.zIndex === '99') {
+    return modalCallbackCloseBtnHandler();
+  } else {
+    return closedSideMenu();
+  }
+};
 
-modalCloseFeedback.addEventListener('click', modalCloseFeedbackHandler);
+//закрытие на ESC
+function escapeKeydownHandler(evt) {
+  if (evt.keyCode === 27) {
+    closeModal();
+  }
+};
+
+navigationOpenBtn.addEventListener('click', openedSideMenu);
+navigationCloseBtn.addEventListener('click', closedSideMenu);
+
+wrapperOpenMenu.addEventListener('click', closeModal);
+
+modalCallbackCloseBtn.addEventListener('click', modalCallbackCloseBtnHandler);
+modalFeedbackCloseBtn.addEventListener('click', modalFeedbackCloseBtnHandler);
