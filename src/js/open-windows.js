c16951dd7d732f17126d0595e1a3c413e5cb4aeb
Lev Karpov <nt66@bk.ru>
2020-08-22 18:15:34 +0300

    исправление

--- src/js/open-windows.js
+++ src/js/open-windows.js
@@ -1,7 +1,7 @@
 const navigationOpenBtn = document.querySelector('.header__navigation-toggle-btn');
 const navigationCloseBtn = document.querySelector('.side-menu__toggle-btn');
 const sideMenu = document.querySelector('.side-menu');
-const wrapperOpenMenu = document.querySelector('.overlay');
+const overlay = document.querySelector('.overlay');
 
 const modalFeedbackOpenBtn = document.querySelectorAll('.contacts__link--feedback');
 const modalFeedback = document.querySelector('.modal_feedback');
@@ -17,38 +17,38 @@ const callbackInputFocus = modalCallback.querySelector('.form__input');
 // открывает боковое меню
 function openedSideMenu() {
   sideMenu.classList.add('side-menu__active');
-  wrapperOpenMenu.classList.add('overlay--active');
+  overlay.classList.add('overlay--active');
   document.addEventListener('keydown', escapeKeydownHandler);
 }
 
 // закрывает боковое меню
 function closedSideMenu() {
   sideMenu.classList.remove('side-menu__active');
-  wrapperOpenMenu.classList.remove('overlay--active');
+  overlay.classList.remove('overlay--active');
 }
  
 //функции для модалок
 function closeModalBg() {
   if (sideMenu.classList.contains('side-menu__active')) {
-    wrapperOpenMenu.style.zIndex = '9';
+    overlay.style.zIndex = '9';
  } else {
-  wrapperOpenMenu.classList.remove('overlay--active');
-  wrapperOpenMenu.removeAttribute('style');
+  overlay.classList.remove('overlay--active');
+  overlay.removeAttribute('style');
   }
 }
 
 function feedbackBtnOpenHandler() {
   modalFeedback.classList.add('modal_feedback--active');
-  wrapperOpenMenu.classList.add('overlay--active');
-  wrapperOpenMenu.style.zIndex = '99';
+  overlay.classList.add('overlay--active');
+  overlay.style.zIndex = '99';
   feedbackInputFocus.focus();
   document.addEventListener('keydown', escapeKeydownHandler);
 }
 
 function callbackBtnOpenHandler() {
   modalCallback.classList.add('modal_callback--active');
-  wrapperOpenMenu.classList.add('overlay--active');
-  wrapperOpenMenu.style.zIndex = '99';
+  overlay.classList.add('overlay--active');
+  overlay.style.zIndex = '99';
   callbackInputFocus.focus();
   document.addEventListener('keydown', escapeKeydownHandler);
 }
@@ -65,7 +65,7 @@ function modalCallbackCloseBtnHandler() {
 
 for (let j = 0; j < modalFeedbackOpenBtn.length; j++) {
   modalFeedbackOpenBtn[j].addEventListener('click',feedbackBtnOpenHandler);
-} 
+}
 
 for (let i = 0; i < modalCallbackOpenBtn.length; i++) {
   modalCallbackOpenBtn[i].addEventListener('click', callbackBtnOpenHandler)
@@ -73,9 +73,10 @@ for (let i = 0; i < modalCallbackOpenBtn.length; i++) {
 
 //фнкция закрытия при esc и клике по фону
 function closeModal() {
-  if (modalFeedback.classList.contains('modal_feedback--active') && wrapperOpenMenu.style.zIndex === '99') {
-    return modalFeedbackCloseBtnHandler();
-  } if (modalCallback.classList.contains('modal_callback--active') && wrapperOpenMenu.style.zIndex === '99') {
+ if (modalFeedback.classList.contains('modal_feedback--active') && overlay.style.zIndex === '99') {
+   return modalFeedbackCloseBtnHandler();
+  }
+   if (modalCallback.classList.contains('modal_callback--active') && overlay.style.zIndex === '99') {
     return modalCallbackCloseBtnHandler();
   } else {
     return closedSideMenu();
@@ -92,7 +93,7 @@ function escapeKeydownHandler(evt) {
 navigationOpenBtn.addEventListener('click', openedSideMenu);
 navigationCloseBtn.addEventListener('click', closedSideMenu);
 
-wrapperOpenMenu.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
 modalCallbackCloseBtn.addEventListener('click', modalCallbackCloseBtnHandler);
 modalFeedbackCloseBtn.addEventListener('click', modalFeedbackCloseBtnHandler);
