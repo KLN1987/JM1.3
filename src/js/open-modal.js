62090e07137ab46ae029a58080e398a74bccb137
Lev Karpov <nt66@bk.ru>
2020-08-29 18:55:10 +0300

    исправление

--- src/js/open-modal.js
+++ src/js/open-modal.js
@@ -1,11 +1,11 @@
 const modalFeedbackOpenBtn = document.querySelectorAll('.contacts__link--feedback');
-const modalFeedback = document.querySelector('.modal-feedback');
-const modalFeedbackCloseBtn = document.querySelector('.modal__close-feedback');
+const modalFeedback = document.querySelector('.modal--feedback');
+const modalFeedbackCloseBtn = document.querySelector('.modal__close--feedback');
 const overlayModal = document.querySelector('.overlay--modal');
 
 const modalCallbackOpenBtn = document.querySelectorAll('.contacts__link--callback');
-const modalCallback = document.querySelector('.modal-callback');
-const modalCallbackCloseBtn = document.querySelector('.modal__close-callback');
+const modalCallback = document.querySelector('.modal--callback');
+const modalCallbackCloseBtn = document.querySelector('.modal__close--callback');
 
 const callBackModalFirstInput = modalCallback.querySelector('.form__input');
 const feedBackModalFirstInput = modalFeedback.querySelector('.form__input');
@@ -18,11 +18,11 @@ const ESC_KЕYCODE = 27;
 export function escKeydownHandler(evt) {
   if (evt.keyCode === ESC_KЕYCODE) {
     console.log('aaa');
-    if (modalFeedback.classList.contains('modal-feedback--active')) {
+    if (modalFeedback.classList.contains('modal--feedback--active')) {
       closeFeedbackModal();
       return;
     }
-    else if (modalCallback.classList.contains('modal-callback--active')) {
+    else if (modalCallback.classList.contains('modal--callback--active')) {
       closeCallbackModal();
       return;
     }
@@ -38,38 +38,42 @@ export function escKeydownHandler(evt) {
 
 //открывает модальное окно с обратной связью
 function openFeedbackModal() {
-  modalFeedback.classList.add('modal-feedback--active');
+  modalFeedback.classList.add('modal--feedback--active');
   overlayModal.classList.add('overlay--active');
   feedBackModalFirstInput.focus();
   modalFeedbackCloseBtn.addEventListener('click', feedbackModalCloseButtonClickHandler);
   overlayModal.addEventListener('click', feedbackModalCloseButtonClickHandler);
   document.addEventListener('keydown', escKeydownHandler);
+  //modalCallback.style.display = 'none';
 }
 
 //закрывает модальное окно с обратной связью
 function closeFeedbackModal() {
-  modalFeedback.classList.remove('modal-feedback--active');
+  modalFeedback.classList.remove('modal--feedback--active');
   overlayModal.classList.remove('overlay--active');
   modalFeedbackCloseBtn.removeEventListener('click', feedbackModalCloseButtonClickHandler);
   overlayModal.removeEventListener('click', feedbackModalCloseButtonClickHandler);
+  //modalCallback.style.display = 'block';
 }
 
 //открывает модальное окно с обратным звонком
 function openCallbackModal() {
-  modalCallback.classList.add('modal-callback--active');
+  modalCallback.classList.add('modal--callback--active');
   overlayModal.classList.add('overlay--active');
   callBackModalFirstInput.focus();
   modalCallbackCloseBtn.addEventListener('click', callbackModalCloseButtonClickHandler);
   overlayModal.addEventListener('click', callbackModalCloseButtonClickHandler);
   document.addEventListener('keydown', escKeydownHandler);
+  //modalFeedback.style.display = 'none';
 }
 
 //закрывает модальное окно с обратным звонком
 function closeCallbackModal() {
-  modalCallback.classList.remove('modal-callback--active');
+  modalCallback.classList.remove('modal--callback--active');
   overlayModal.classList.remove('overlay--active');
   modalCallbackCloseBtn.removeEventListener('click', callbackModalCloseButtonClickHandler);
   overlayModal.removeEventListener('click', callbackModalCloseButtonClickHandler);
+  //modalFeedback.style.display = 'block';
 }
 
 //функции для слушателей
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,7 +1,11 @@
-var mySwiper = undefined;
+import Swiper, {Pagination} from 'swiper';
+
+Swiper.use([Pagination]);
+
+let mySwiper;
 function initSwiper() {
-  let screenWidth = screen.availWidth;
-  if ( (screenWidth < 768) && (mySwiper == undefined)) {
+  const screenWidth = document.body.clientWidth;
+  if ((screenWidth < 768) && (mySwiper == undefined)) {
     mySwiper = new Swiper('.swiper-container', {
       slidesPerView: 'auto',
       spaceBetween: 16,
@@ -11,15 +15,64 @@ function initSwiper() {
         clickable: true,
       },
     });
-  } else if ((screenWidth >= 768) && (mySwiper != undefined)) {
-    mySwiper.destroy();
-    mySwiper = undefined;
-    document.querySelector('.swiper-wrapper').removeAttribute('style');
-    /*document.querySelector('.swiper-slide').style.height = 'auto';*/
+  } if ((screenWidth >= 768) && (mySwiper !== undefined)) {
+    for (let i = 0; i < mySwiper.length; i++) {
+    mySwiper[i].destroy(true, true);  
   }
-}
+    mySwiper = undefined;
+      }
+    }
 initSwiper();
 
 window.addEventListener('resize', function() {
   initSwiper();
 });
+
+/*
+(function() {
+
+  'use strict';
+  const breakpoint = window.matchMedia( '(min-width:768px)' );
+
+  let mySwiper;
+
+ 
+  const breakpointChecker = function() {
+    // if larger viewport and multi-row layout needed
+    if ( breakpoint.matches === true ) {
+
+    for (let i = 0; i < mySwiper.length; i++)  {  
+      // clean up old instances and inline styles when available
+	  if ( mySwiper[i] !== undefined ) mySwiper[i].destroy(false, true);
+
+	  // or/and do nothing
+	  return;
+
+      // else if a small viewport and single column layout needed
+      } if ( breakpoint.matches === false ) {
+
+        // fire small viewport version of swiper
+        return enableSwiper();
+
+      }
+    }
+  };
+
+  const enableSwiper = function() {
+    mySwiper = new Swiper ('.swiper-container', {
+      slidesPerView: 'auto',
+      spaceBetween: 16,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+      },
+    });
+  };
+
+  breakpoint.addListener(breakpointChecker);
+
+  // kickstart
+  breakpointChecker();
+
+})();*/
